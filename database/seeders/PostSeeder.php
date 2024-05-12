1eedb3d408cced398b97e57090fe257d9f254a64
Uwakmfon1 <udohuwakmfon2@gmail.com>
2024-05-12 06:12:27 +0100

    initial commit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // factory(App\Models\Post::class,50)->create();
+    }
+}
